c8742f0ae52f65e8d1a6229cab3d28727f468cc9
serhatprogramming <serhatprogramming@gmail.com>
2023-05-16 14:15:46 -0400

    front end ny type entry, router added

--- src/types.ts
+++ src/types.ts
@@ -7,8 +7,11 @@ export interface Diagnosis {
 export enum Gender {
   Male = "male",
   Female = "female",
-  Other = "other"
+  Other = "other",
 }
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface Entry {}
 
 export interface Patient {
   id: string;
@@ -17,6 +20,9 @@ export interface Patient {
   gender: Gender;
   ssn?: string;
   dateOfBirth?: string;
+  entries: Entry[];
 }
+
+export type NonSensitivePatient = Omit<Patient, "ssn" | "entries">;
 
 export type PatientFormValues = Omit<Patient, "id" | "entries">;
