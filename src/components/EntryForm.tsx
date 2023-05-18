13720e8b074c4d2aed04524e2850f10599aa71b3
serhatprogramming <serhatprogramming@gmail.com>
2023-05-18 15:47:45 -0400

    consolelog removed

--- src/components/EntryForm.tsx
+++ src/components/EntryForm.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { FormControl, Select, MenuItem, InputLabel } from "@mui/material";
 import { SelectChangeEvent } from "@mui/material";
-// import { HealthCheckRating } from "../types";
 import MultipleSelect from "./MultipleDiagnoseSelect";
 import { Diagnosis, Entry, EntryWithoutId, HealthCheckRating } from "../types";
 
